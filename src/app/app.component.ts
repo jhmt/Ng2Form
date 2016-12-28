a626d118e87e231eda9b26b13ea173f5a6f119dd
jhmt <jhashimot00@gmail.com>
2016-12-28 14:22:49 +1300

    updating app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+// Angular
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validator } from '@angular/forms';
+
+// Interfaces:
+import { User } from './user.interface';
 
 @Component({
-  selector: 'app-root',
+  moduleId: module.id,
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  public formGroup: FormGroup;
+  public submitted: boolean;
+  public events: any[] = [];
+
+  constructor (private _fb: FormBuilder) {
+  }
+
+  ngOnInit (): void {
+
+  }
+
+  saveUser (user: User, isValid: boolean) {
+    this.submitted = true;
+    alert(`${user.name} saved`);
+  }
 }
